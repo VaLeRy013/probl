b6fe4789020a4c9761bb5500442cc54c546ac3d3
Valeria <70991795+VaLeRy013@users.noreply.github.com>
2020-10-06 19:41:42 +0300

    Add files via upload

---
+++ problinfo12.py
@@ -0,0 +1,9 @@
+"""Într-o gospodărie sunt 4 găini. Introduceţi în calculator prin variabilele a, b, c, d
+numărul de ouă pe care-l dă fiecare găină într-o zi. Afişaţi câte ouă se obţin într-o
+săptămână.
+"""
+og1 = int(input("oua facute de 1 gaina in fiecare zi: "))
+og2 = int(input("oua facute de 2 gaina in fiecare zi: "))
+og3 = int(input("oua facute de 3 gaina in fiecare zi: "))
+og4 = int(input("oua facute de 4 gaina in fiecare zi: "))
+print("Numarul de oua facute de toate gainile impreuna este: ", 7 * (og1 + og2 + og3 + og4))
