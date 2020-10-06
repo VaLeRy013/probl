4b3d048a0421ec8598e984ede705c7f60ac7eae1
Valeria <70991795+VaLeRy013@users.noreply.github.com>
2020-10-06 19:34:38 +0300

    Delete problinfo3.py

--- problinfo3.py
+++
@@ -1,7 +0,0 @@
-"""Într-un autobuz care pleacă în excursie sunt 7 copii. De la încă două şcoli urcă alţi
-copii, numărul acestora citindu-se de la tastatura. Câţi copii au plecat în excursie?
-Exemplu: Date de intrare: 15 20 Date de ieşire: 42 copii."""
-st1 = int(input("cati copii au urcat la prima statie: "))
-st2 = int(input("cati copii au urcat la a doua statie: "))
-nrt = st + st2 + 7
-print("In excursie au plecat ",nrt," copii")
