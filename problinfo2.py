eed5adfd5191899754b4de77bbed1e07bf0360c6
Valeria <70991795+VaLeRy013@users.noreply.github.com>
2020-10-06 19:33:21 +0300

    Add files via upload

---
+++ problinfo2.py
@@ -0,0 +1,8 @@
+"""Într-o tabără numărul de băieţi este cu 10 mai mare decât cel al fetelor. Dacă se citeşte
+de la tastatură numărul de fete, să se spună câţi elevi sunt în tabără. Exemplu: date de
+intrare: 50 date de ieşire: 110."""
+
+nrf = int(input("dati numarul de fete: "))
+nrb = nrf + 10
+nrt = nrb + nrf
+print("Numarul total de fete si baieti in tabara este: ",nrt)
